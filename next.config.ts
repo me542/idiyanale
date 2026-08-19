6641eba2000623e597a2603a2952ee1925c0338a
Bakawan-User <bakawan-user@27-TS-RFlor.local>
2026-08-19 15:55:52 +0800

    changed to standalone

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-  /* config options here */
+
+  output: "standalone",
 };
 
 export default nextConfig;
